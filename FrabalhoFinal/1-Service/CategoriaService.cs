e574dc92639ace02e3360a0c800d2e9e751a024d
27breno <brenoferreira1327@gmail.com>
2024-10-21 17:20:19 -0300

    21/10 Categoria
    Aqui eu fiz catergoria em tudo menso na API

---
+++ FrabalhoFinal/1-Service/CategoriaService.cs
@@ -0,0 +1,37 @@
+﻿using FrabalhoFinal._2_Repository;
+using FrabalhoFinal._3_Entidade;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrabalhoFinal._1_Service
+{
+    public class CategoriaService
+    {
+        CategoriaRepository repositorio { get; set; }
+        public CategoriaService(string connectionString)
+        {
+            repositorio = new CategoriaRepository(connectionString);
+        }
+        public void Adicionar(Categoria a)
+        {
+            repositorio.Adicionar(a);
+        }
+
+        public List<Categoria> Listar()
+        {
+            return repositorio.listar();
+        }
+        public void editar(Categoria a)
+        {
+            repositorio.editar(a);
+        }
+        public void Remover(int id)
+        {
+
+            repositorio.Remover(id);
+        }
+    }
+}
---
+++ FrabalhoFinal/3-Entidade/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrabalhoFinal._3_Entidade
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+
+        public string Tipo { get; set; } /* FIlme, Serie, Documentario */
+
+        public string Genero { get; set; }
+    }
+}
