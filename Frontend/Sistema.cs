ec0c3cffed02e2ddb581cbe8867689385edd6e43
27breno <brenoferreira1327@gmail.com>
2024-11-06 17:05:04 -0300

    01/11
    começo do sistema

--- Frontend/Sistema.cs
+++ Frontend/Sistema.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FrabalhoFinal._3_Entidade;
+using Frontend.Models;
+using Frontend.Usecases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +11,55 @@ namespace Frontend
 {
     public class Sistema
     {
+        private readonly AssinaturaUC _assinaturaUC;
+        private readonly AvaliacaoUC _avaliacaoUC;
+        private readonly CategoriaUC _categoriaUC;
+        private readonly ConteudoUC _conteudoUC;
+        private readonly PessoaUC _pessoaUC;
+
+       
         public Sistema(HttpClient  cliente) 
         { 
-            
+            _assinaturaUC = new AssinaturaUC(cliente);
+            _avaliacaoUC = new AvaliacaoUC(cliente);
+            _categoriaUC= new CategoriaUC(cliente);
+            _conteudoUC = new ConteudoUC(cliente);
+            _pessoaUC = new PessoaUC(cliente);
         }
 
         public void IniciarSistema() 
         {
             
         }
-
-        public void FazerAvaliacao()
+        public void MenuPrincipal()
         {
+
+        }
+        public void Login( ) 
+        {
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("digite seu nome");
+            pessoa.Nome = Console.ReadLine();
+
+            Console.WriteLine("digite a sua idade");
+            pessoa.Idade =int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite seu CPF");
+            pessoa.CPF = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite seu Email");
+            pessoa.Email = Console.ReadLine();
+
+            Console.WriteLine("digite a sua senha");
+            pessoa.Senha = Console.ReadLine();
+        }
+
+        public void CriarAvaliacao()
+        {
+            Avaliacao avaliacao= new Avaliacao();
+
+
 
         }
     }
